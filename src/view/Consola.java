7715dfb6618abae91114aa5606b3cc9ddbe53f1e
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-07 21:16:09 +0000

    Cree el método para poder mostrar libros en la consola

--- src/view/Consola.java
+++ src/view/Consola.java
@@ -5,5 +5,11 @@ import model.*;
 import java.util.List;
 
 public class Consola {
-    
+    public static void mostrarLibros(List<Libro> libros) {
+        System.out.println("LIBROS");
+
+        for (Libro l : libros) {
+            System.out.println(l);
+        }
+    }
 }
