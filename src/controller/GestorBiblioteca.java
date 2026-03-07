56848a97a90e2da78be42ef3c3304f36e1d106b4
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-07 20:55:50 +0000

    Cree la clase GestorBiblioteca con la lista de datos

---
+++ src/controller/GestorBiblioteca.java
@@ -0,0 +1,12 @@
+package controller;
+
+import model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestorBiblioteca {
+    private List<Libro> libros;
+    private List<Usuario> usuarios;
+    private List<Prestamos> prestamos;
+}
