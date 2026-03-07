777d5a202ea23e5cecdf83e789fd7f67264a9a2b
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-07 20:34:17 +0000

    Cree la clase Libro con sus atributos

---
+++ src/model/Libro.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Libro {
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private Genero genero;
+
+    private int copiasTotales;
+    private int copiasDisponibles;
+
+    private EstadoLibro estado;
+}
