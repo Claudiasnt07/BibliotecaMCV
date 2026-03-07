e0b4085e15abbcb008c458f66da29e9544b80b2f
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-07 20:48:44 +0000

    Cree la clase Prestamo con sus atributos

---
+++ src/model/Prestamo.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Prestamo {
+    private Libro libro;
+    private Usuario usuario;
+
+    private LocalDate fechaPrestamo;
+    private LocalDate fechaVencimiento;
+}
