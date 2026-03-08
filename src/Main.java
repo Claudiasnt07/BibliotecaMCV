ce22a149b58c2fbffbfe1ca3da33bdb6efe32e45
Aythami <aythamimayorsanchez@gmail.com>
2026-03-08 14:56:39 +0100

    Creo el Main con los objetos

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
        GestorBiblioteca gestor = new GestorBiblioteca();
 
-       Libro libro1 = new Libro("1","El Hobbit","Tolkien",Genero.FANTASIA,2);
+       Libro libro1 = new Libro("1","Sharknado","Pedro",Genero.FANTASIA,2);
        Usuario usuario1 = new Usuario(1,"Ana");
 
        gestor.agregarLibro(libro1);
